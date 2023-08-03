6fa9f66d79a3e2fe329bc98497dc3b734023eead
jeevitha160 <140681573+jeevitha160@users.noreply.github.com>
2023-08-03 14:28:13 +0530

    Update settings.py

--- photo/settings.py
+++ photo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-efb8&peka#$v__h(5efb)ixy5%q+gg=f-2pin(^b6b(b@5i%i2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jeevi123.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
@@ -129,3 +130,4 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
